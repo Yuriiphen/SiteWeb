55ff9dd780e1c681c62a0dcd09333960bf7be353
Yuriiphen <yuriijohnny@gmail.com>
2026-01-15 14:25:49 +0100

    Ajout pages account&edit password

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,8 +5,12 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/Lacarte", "La carte", "/pages/Lacarte.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html"),
-    new Route("/signup", "Inscription", "/pages/signup.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
+
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
