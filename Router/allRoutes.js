1f5e921d068a139f15765ca9392dd4e0e060325e
Yuriiphen <yuriijohnny@gmail.com>
2026-01-15 13:36:52 +0100

    Ajout page Signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/Lacarte", "La carte", "/pages/Lacarte.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
