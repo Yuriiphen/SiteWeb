a1428957a9bdc0168dc7a976f5fda1ff240a942c
Yuriiphen <yuriijohnny@gmail.com>
2026-01-15 15:21:32 +0100

    Gestion de l'affichage de réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/reservation", "Vos réservations", "/pages/reservation/allResa.html"),
 
 
 ];
