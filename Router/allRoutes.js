977f79e118fb421a99af4388be12db6f79c3f5f5
Yuriiphen <yuriijohnny@gmail.com>
2026-01-20 10:35:05 +0100

    Modifs générales + ajout roles et autorisations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,19 +2,19 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/Lacarte", "La carte", "/pages/Lacarte.html"),
-    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
-    new Route("/account", "Mon compte", "/pages/auth/account.html"),
-    new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html"),
-    new Route("/reservation", "Vos réservations", "/pages/reservation/allResa.html"),
-    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
-    new Route("/Annulation", "Annuler une réservation", "/pages/reservation/Annulation.html"),
-    new Route("/ModifierResa", "Modifier une réservation", "/pages/reservation/ModifierResa.html"),
-    new Route("/validAnnul", "Annulation confirmée", "/pages/reservation/validAnnul.html"),
-    new Route("/confirmAnnul", "Confirmer l'annulation", "/pages/reservation/confirmAnnul.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", []),
+    new Route("/Lacarte", "La carte", "/pages/Lacarte.html", []),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", "/pages/js/auth/signin.js", ["disconnected"]),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", "/pages/js/auth/signup.js", ["disconnected"]),
+    new Route("/account", "Mon compte", "/pages/auth/account.html", ["client","admin"]),
+    new Route("/editPassword", "Modifier votre mot de passe", "/pages/auth/editPassword.html", ["client","admin"]),
+    new Route("/reservation", "Vos réservations", "/pages/reservation/allResa.html", ["client"]),
+    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html", ["client"]),
+    new Route("/Annulation", "Annuler une réservation", "/pages/reservation/Annulation.html", ["client"]),
+    new Route("/ModifierResa", "Modifier une réservation", "/pages/reservation/ModifierResa.html", ["client"]),
+    new Route("/validAnnul", "Annulation confirmée", "/pages/reservation/validAnnul.html", ["client"]),
+    new Route("/confirmAnnul", "Confirmer l'annulation", "/pages/reservation/confirmAnnul.html", ["client"]),
 
 
 
